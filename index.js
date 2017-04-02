1ca3cad2dfd5f2524cdee9d6f22580c81f43e5c8
your name <your_email@youremail.com>
2017-04-02 15:16:53 +0800

    删除多余代码

--- index.js
+++ index.js
@@ -3,16 +3,3 @@ button.addEventListener("click", function() {
         document.querySelectorAll('.right span')[i].innerHTML = document.querySelectorAll('.left input')[i].value;
     }
 })
-
-
-http: //js.jirengu.com/jovukinune/2/edit
-
-
-    function assert(value, description) {
-        value = value || '';
-        if (value === true) {
-            console.info('[success]');
-        } else {
-            console.error('[fail]:' + description);
-        }
-    }
